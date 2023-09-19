02bc7448fdcf9e0031b180c7c29fbd343e273a3b
domvillegas <domvillgithub@gmail.com>
2023-09-19 10:15:37 -0700

    Add intro fade. Route to menu.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./index.module.scss";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const route = useRouter();
+
+  useEffect(() => {
+    const pageContainer = document.getElementsByClassName("fadeIn")[0];
+
+    setTimeout(() => {
+      pageContainer.classList.remove("fadeIn");
+    }, 1500);
+  });
+
   return (
-    <div className={styles.home}>
+    <div className={`${styles.home} fadeIn`}>
       <div className={styles.contentContainer}>
         <h1 className={styles.domText}>dom</h1>
-        <h1 className={styles.enterText}>enter</h1>
+        <h1 className={styles.enterText} onClick={() => route.push("/menu")}>
+          enter
+        </h1>
       </div>
     </div>
   );
---
+++ src/pages/menu/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Menu = () => {
+  return <div>lmao</div>;
+};
+
+export default Menu;
