35c7aad551e1356148652b11e42cb0b61c445b20
domvillegas <domvillgithub@gmail.com>
2023-09-25 10:57:18 -0700

    Add favicon.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>dom</title>
         <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💭</text></svg>"
+        ></link>
+
+        <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         ></link>
