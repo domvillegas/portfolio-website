42e2326aad39b3c5bd8a9dcc2dfd0c157ef8e291
domvillegas <domvillgithub@gmail.com>
2023-09-27 13:03:33 -0700

    Add resting expansion animation

--- src/pages/playground/field/components/Stars.tsx
+++ src/pages/playground/field/components/Stars.tsx
@@ -2,35 +2,39 @@ import React from "react";
 import styles from "./Stars.module.scss";
 import { starsPositions } from "../starsPositions";
 
-const Star = () => {
-  return starsPositions.map((starPosition, index) => {
-    return (
-      <div
-        onClick={() => window.alert(`Star ${index}`)}
-        className={styles.star}
-        key={index}
-        style={{
-          position: "fixed",
-          top: starPosition.top,
-          left: starPosition.left,
-        }}
-      >
-        <div
-          style={{ animationDelay: `${5 + index * 0.025}s` }}
-          className={`${styles.objectContainer} ${styles.twinkle}`}
-        >
+const Stars = () => {
+  return (
+    <div className={styles.stars}>
+      {starsPositions.map((starPosition, index) => {
+        return (
           <div
-            style={{ animationDelay: `${index * 0.025}s` }}
-            className={`${styles.object} ${
-              index % 2
-                ? `${styles.objectA} ${styles.rotateSquish}`
-                : styles.objectB
-            } ${styles.rotateSquish}`}
-          />
-        </div>
-      </div>
-    );
-  });
+            onClick={() => window.alert(`Star ${index}`)}
+            className={styles.star}
+            key={index}
+            style={{
+              position: "fixed",
+              top: starPosition.top,
+              left: starPosition.left,
+            }}
+          >
+            <div
+              style={{ animationDelay: `${5 + index * 0.025}s` }}
+              className={`${styles.objectContainer} ${styles.twinkle}`}
+            >
+              <div
+                style={{ animationDelay: `${index * 0.025}s` }}
+                className={`${styles.object} ${
+                  index % 2
+                    ? `${styles.objectA} ${styles.rotateSquish}`
+                    : styles.objectB
+                } ${styles.rotateSquish}`}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
-export default Star;
+export default Stars;
--- src/pages/playground/field/index.tsx
+++ src/pages/playground/field/index.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import styles from "./Field.module.scss";
-import Star from "./components/Stars";
+import Stars from "./components/Stars";
 
 const Field = () => {
   const [summoned, setSummoned] = useState(false);
 
   return (
     <div className={styles.field}>
-      {summoned && <Star />}
+      {summoned && <Stars />}
       {!summoned && (
         <button type="button" onClick={() => setSummoned(true)}>
           open
