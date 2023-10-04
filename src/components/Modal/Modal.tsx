94a7748cc9956ae8e9dfb28a7787e4be80f04cc8
domvillegas <domvillgithub@gmail.com>
2023-10-04 10:21:31 -0700

    Add Modal content.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler } from "react";
+import React, { MouseEventHandler, ReactNode } from "react";
 import styles from "./Modal.module.scss";
 import { Star } from "@/utils/types";
 
@@ -6,16 +6,22 @@ interface Props {
   visible: boolean;
   close: MouseEventHandler<HTMLButtonElement>;
   data: {} | Star;
+  markup: ReactNode;
 }
 
-const Modal = ({ visible = false, close, data }: Props) => {
+const Modal = ({ visible = false, close, data, markup }: Props) => {
   return (
     <div
       className={`${styles.modal} ${visible ? styles.reveal : styles.hide}
       `}
     >
-      <p>{(data as Star).note}</p>
-      <button onClick={close}>close</button>
+      <div className={styles.buttonContainer}>
+        <button onClick={close} type="button" className={styles.closeButton}>
+          <div className={styles.closeButtonArmOne} />
+          <div className={styles.closeButtonArmTwo} />
+        </button>
+      </div>
+      {markup}
     </div>
   );
 };
--- src/pages/playground/field/index.tsx
+++ src/pages/playground/field/index.tsx
@@ -11,6 +11,8 @@ const Field = () => {
   const [summonedClicked, setSummonedClicked] = useState(false);
   const [stars, setStars] = useState<[] | Star[]>([]);
   const [selectedStar, setSelectedStar] = useState<{} | Star>([]);
+  const [starName, setStarName] = useState("");
+  const [starDescription, setStarDescription] = useState("");
 
   useEffect(() => {
     const getStarData = async () => {
@@ -47,12 +49,42 @@ const Field = () => {
     }
   };
 
+  const modalContent = (
+    <div className={styles.starForm}>
+      <form>
+        {(selectedStar as Star).name ? (
+          <span>{(selectedStar as Star).name}</span>
+        ) : (
+          <input
+            type="name"
+            placeholder="Star Name"
+            onChange={(event) => setStarName(event.target.value)}
+          />
+        )}
+        {(selectedStar as Star).note ? (
+          <span>{(selectedStar as Star).note}</span>
+        ) : (
+          <input
+            type="text"
+            placeholder="Star Description"
+            onChange={(event) => setStarDescription(event.target.value)}
+          />
+        )}
+
+        <button type="submit">
+          {(selectedStar as Star).editTime ? "Alter" : "Claim"}
+        </button>
+      </form>
+    </div>
+  );
+
   return (
     <div className={styles.field}>
       {summoned ? (
         <>
           <Stars action={starClickHandler} data={stars} />
           <Modal
+            markup={modalContent}
             visible={(selectedStar as any).id ? true : false}
             close={() => setSelectedStar([])}
             data={selectedStar}
